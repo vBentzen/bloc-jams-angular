0fffa622f6e0bc84b1ae2d429f01a3aa293d1475
Aske Bentzen <askebentzen@gmail.com>
2017-02-01 10:08:37 +0100

    created player-bar template and added on/off hover play/pause btn on songs

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -7,7 +7,7 @@
       });
     $stateProvider
         .state('landing', {
-          url: '/landing',
+          url: '/',
           templateUrl: '/templates/landing.html'
         })
         .state('album', {
