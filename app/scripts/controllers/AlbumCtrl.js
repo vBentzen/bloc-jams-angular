e6cd4dfcaee9cf485879fde7bbdbe15f5f27f111
Aske Bentzen <askebentzen@gmail.com>
2017-02-08 18:17:51 +0100

    continuecp5

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,11 +1,8 @@
 (function() {
     function AlbumCtrl() {
-      this.albumData = [];
-      for (var i = 0; i < 12; i++) {
-        this.albumData.push(angular.copy(albumPicasso));
-      }
+      this.albumData = angular.copy(albumPicasso);
     }
     angular
     .module('blocJams')
-    .controller('AlbumCtrl', albumCtrl);
+    .controller('AlbumCtrl', AlbumCtrl);
 })();
