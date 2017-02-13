697ba95877f7ba043787665aaa8d29c6d2f1b100
Aske Bentzen <askebentzen@gmail.com>
2017-02-13 21:38:52 +0100

    added logic for seekbar so it follows song

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function() {
-  function SongPlayer(Fixtures){
+  function SongPlayer($rootScope, Fixtures){
     var SongPlayer = {};
 
     /**
@@ -50,6 +50,12 @@
         preload: true
       });
 
+      currentBuzzObject.bind('timeupdate', function functionName() {
+        $rootScope.$apply(function functionName() {
+          SongPlayer.currentTime = currentBuzzObject.getTime();
+        });
+      });
+
       SongPlayer.currentSong = song;
     };
 
@@ -69,6 +75,11 @@
     */
     SongPlayer.currentSong = null;
 
+    /**
+    * @desc Current playback time (in seconds) of currently playing song
+    * @type {Number}
+    */
+    SongPlayer.currentTime = null;
 
     /**
     * @function play
@@ -129,9 +140,16 @@
 
     };
 
-
-
-
+    /**
+    * @function setCurrentTime
+    * @desc Set current time (in seconds) of currently playing song
+    * @param {Number} time
+    */
+    SongPlayer.setCurrentTime = function(time) {
+      if (currentBuzzObject) {
+        currentBuzzObject.setTime(time);
+      }
+    };
 
 
 
@@ -140,5 +158,5 @@
 
   angular
   .module('blocJams')
-  .factory('SongPlayer',['Fixtures', SongPlayer]);
+  .factory('SongPlayer',['$rootScope', 'Fixtures', SongPlayer]);
 })();
